f00fe3cfab9d953bacb8d45bee16558852cf33da
Zeeshan Abbasi <zeeshan@mparsec.com>
2020-04-11 15:00:25 +0500

    --remved commented code

--- ControllerSourceCode/ArduinoVent/ArduinoVent.ino
+++ ControllerSourceCode/ArduinoVent/ArduinoVent.ino
@@ -2,18 +2,6 @@
 #include "hal.h"
 #include <SoftwareSerial.h>
 
-// SoftwareSerial *monitor;
-
-// bool sendToMonitor(char *data, int dataLen)
-// {
-
-//   if (monitor->available())
-//   {
-//     for (int i = 0; i < dataLen; i++)
-//       monitor->write(data[i]);
-//   }
-// }
-
 //#define GET_RESET_VAL // disable as this code is not working
 #ifdef GET_RESET_VAL
 uint8_t resetFlags __attribute__((section(".noinit")));
@@ -32,12 +20,8 @@ void resetFlagsInit()
 uint8_t resetFlags = 0;
 #endif
 
-//getPsi(A5); //PIN 8
-//float *vals=new float;
 void setup()
 {
-  // monitor=new SoftwareSerial(19,18);//Serial data to Monitor
-  // monitor->begin(19200);
   resetFlagsInit();
   halInit(resetFlags);
   ventSetup();
